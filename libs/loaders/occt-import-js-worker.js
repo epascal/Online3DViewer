4e3de664844fceb5184a72057eb87993ee1ac2cb
epascal <eric@pascal-lab.ch>
2022-12-05 16:35:37 +0100

    add triangulation parameters

--- libs/loaders/occt-import-js-worker.js
+++ libs/loaders/occt-import-js-worker.js
@@ -4,12 +4,13 @@ onmessage = async function (ev)
 {
 	let occt = await occtimportjs ();
 	let result = null;
+	let params = ev.data.format ? ev.data.format : null;
 	if (ev.data.format === 'step') {
-		result = occt.ReadStepFile (ev.data.buffer);
+		result = occt.ReadStepFile (ev.data.buffer, params);
 	} else if (ev.data.format === 'iges') {
-		result = occt.ReadIgesFile (ev.data.buffer);
+		result = occt.ReadIgesFile (ev.data.buffer, params);
 	} else if (ev.data.format === 'brep') {
-		result = occt.ReadBrepFile (ev.data.buffer);
+		result = occt.ReadBrepFile (ev.data.buffer, params);
 	}
 	postMessage (result);
 };
--- source/engine/import/importerocct.js
+++ source/engine/import/importerocct.js
@@ -6,47 +6,40 @@ import { ConvertThreeGeometryToMesh } from '../threejs/threeutils.js';
 import { ImporterBase } from './importerbase.js';
 import { ColorToMaterialConverter } from './importerutils.js';
 
-export class ImporterOcct extends ImporterBase
-{
-    constructor ()
-    {
-        super ();
-		this.worker = null;
-    }
-
-    CanImportExtension (extension)
-    {
-        return extension === 'stp' || extension === 'step' || extension === 'igs' || extension === 'iges' || extension === 'brp' || extension === 'brep';
-    }
-
-    GetUpDirection ()
-    {
-        return Direction.Y;
-    }
-
-	ClearContent ()
-	{
-        if (this.worker !== null) {
-            this.worker.terminate ();
-            this.worker = null;
-        }
-	}
-
-    ResetContent ()
-    {
+export class ImporterOcct extends ImporterBase {
+    constructor() {
+        super();
         this.worker = null;
     }
 
-    ImportContent (fileContent, onFinish)
-    {
-        let workerPath = GetExternalLibPath ('loaders/occt-import-js-worker.js');
-        this.worker = new Worker (workerPath);
-        this.worker.addEventListener ('message', (ev) => {
-            this.ImportResultJson (ev.data, onFinish);
+    CanImportExtension(extension) {
+        return extension === 'stp' || extension === 'step' || extension === 'igs' || extension === 'iges' || extension === 'brp' || extension === 'brep';
+    }
+
+    GetUpDirection() {
+        return Direction.Y;
+    }
+
+    ClearContent() {
+        if (this.worker !== null) {
+            this.worker.terminate();
+            this.worker = null;
+        }
+    }
+
+    ResetContent() {
+        this.worker = null;
+    }
+
+    ImportContent(fileContent, onFinish) {
+        let workerPath = GetExternalLibPath('loaders/occt-import-js-worker.js');
+        this.worker = new Worker(workerPath);
+        this.worker.addEventListener('message', (ev) => {
+            this.ImportResultJson(ev.data, onFinish);
         });
-        this.worker.addEventListener ('error', (ev) => {
-            this.SetError ('Failed to load occt-import-js.');
-            onFinish ();
+        this.worker.addEventListener('error', (ev) => {
+            this.SetError('Failed to load occt-import-js.');
+            onFinish();
         });
 
         let format = null;
@@ -57,63 +50,64 @@ export class ImporterOcct extends ImporterBase
         } else if (this.extension === 'brp' || this.extension === 'brep') {
             format = 'brep';
         } else {
-            onFinish ();
+            onFinish();
             return;
         }
 
-        let fileBuffer = new Uint8Array (fileContent);
-        this.worker.postMessage ({
-            format : format,
-            buffer : fileBuffer
+        let fileBuffer = new Uint8Array(fileContent);
+        this.worker.postMessage({
+            format: format,
+            buffer: fileBuffer,
+            params: {
+                linearDeflection: 0.01,
+                angularDeflection: 0.17
+            },
         });
     }
 
-	ImportResultJson (resultContent, onFinish)
-	{
+    ImportResultJson(resultContent, onFinish) {
         if (!resultContent.success) {
-            onFinish ();
+            onFinish();
             return;
         }
-        let colorToMaterial = new ColorToMaterialConverter (this.model);
-        let rootNode = this.model.GetRootNode ();
-        this.ImportNode (resultContent, resultContent.root, rootNode, colorToMaterial);
-        onFinish ();
-	}
+        let colorToMaterial = new ColorToMaterialConverter(this.model);
+        let rootNode = this.model.GetRootNode();
+        this.ImportNode(resultContent, resultContent.root, rootNode, colorToMaterial);
+        onFinish();
+    }
 
-    ImportNode (resultContent, occtNode, parentNode, colorToMaterial)
-    {
+    ImportNode(resultContent, occtNode, parentNode, colorToMaterial) {
         for (let nodeMeshIndex of occtNode.meshes) {
             let occtMesh = resultContent.meshes[nodeMeshIndex];
-            let mesh = this.ImportMesh (occtMesh, colorToMaterial);
-            let meshIndex = this.model.AddMesh (mesh);
-            parentNode.AddMeshIndex (meshIndex);
+            let mesh = this.ImportMesh(occtMesh, colorToMaterial);
+            let meshIndex = this.model.AddMesh(mesh);
+            parentNode.AddMeshIndex(meshIndex);
         }
         for (let childOcctNode of occtNode.children) {
-            let childNode = new Node ();
-            childNode.SetName (childOcctNode.name);
-            parentNode.AddChildNode (childNode);
-            this.ImportNode (resultContent, childOcctNode, childNode, colorToMaterial);
+            let childNode = new Node();
+            childNode.SetName(childOcctNode.name);
+            parentNode.AddChildNode(childNode);
+            this.ImportNode(resultContent, childOcctNode, childNode, colorToMaterial);
         }
     }
 
-    ImportMesh (occtMesh, colorToMaterial)
-    {
+    ImportMesh(occtMesh, colorToMaterial) {
         let materialIndex = null;
         if (occtMesh.color) {
-            let color = RGBColorFromFloatComponents (occtMesh.color[0], occtMesh.color[1], occtMesh.color[2]);
-            materialIndex = colorToMaterial.GetMaterialIndex (color.r, color.g, color.b, null);
+            let color = RGBColorFromFloatComponents(occtMesh.color[0], occtMesh.color[1], occtMesh.color[2]);
+            materialIndex = colorToMaterial.GetMaterialIndex(color.r, color.g, color.b, null);
         }
-        let mesh = ConvertThreeGeometryToMesh (occtMesh, materialIndex);
+        let mesh = ConvertThreeGeometryToMesh(occtMesh, materialIndex);
         if (occtMesh.name) {
-            mesh.SetName (occtMesh.name);
+            mesh.SetName(occtMesh.name);
         }
         if (occtMesh.face_colors) {
             for (let faceColorGroup of occtMesh.face_colors) {
-                let faceColor = RGBColorFromFloatComponents (faceColorGroup.color[0], faceColorGroup.color[1], faceColorGroup.color[2]);
-                let faceMaterialIndex = colorToMaterial.GetMaterialIndex (faceColor.r, faceColor.g, faceColor.b, null);
+                let faceColor = RGBColorFromFloatComponents(faceColorGroup.color[0], faceColorGroup.color[1], faceColorGroup.color[2]);
+                let faceMaterialIndex = colorToMaterial.GetMaterialIndex(faceColor.r, faceColor.g, faceColor.b, null);
                 for (let i = faceColorGroup.first; i <= faceColorGroup.last; i++) {
-                    let triangle = mesh.GetTriangle (i);
-                    triangle.SetMaterial (faceMaterialIndex);
+                    let triangle = mesh.GetTriangle(i);
+                    triangle.SetMaterial(faceMaterialIndex);
                 }
             }
         }
